eb31cc6b6f3d7df82ace71087ad548e2f71f241d
Austin Hughes <ahughes02@live.com>
2014-12-31 18:25:20 -0500

    In theory phone updates data item which watch can read later

--- mobile/src/main/java/net/austinhughes/speedreaderforwear/MainActivity.java
+++ mobile/src/main/java/net/austinhughes/speedreaderforwear/MainActivity.java
@@ -2,37 +2,94 @@ package net.austinhughes.speedreaderforwear;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.common.*;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.wearable.*;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity
+{
+    private GoogleApiClient mGoogleApiClient;
+    private PutDataMapRequest dataMap;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(new GoogleApiClient.ConnectionCallbacks()
+                {
+                    @Override
+                    public void onConnected(Bundle connectionHint)
+                    {
+                        Toast toast = Toast.makeText(getBaseContext(), "Connected to Wear Device", Toast.LENGTH_LONG);
+                        toast.show();
+                        Log.d("Wearable API", "onConnected: " + connectionHint);
+                        // Now you can use the Data Layer API
+                    }
+                    @Override
+                    public void onConnectionSuspended(int cause)
+                    {
+                        Log.d("Wearable API", "onConnectionSuspended: " + cause);
+                    }
+                })
+                .addOnConnectionFailedListener(new GoogleApiClient.OnConnectionFailedListener()
+                {
+                    @Override
+                    public void onConnectionFailed(ConnectionResult result)
+                    {
+                        Log.d("Wearable API", "onConnectionFailed: " + result);
+                    }
+                })
+                // Request access only to the Wearable API
+                .addApi(Wearable.API)
+                .build();
+
+        dataMap = PutDataMapRequest.create("/data");
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_settings)
+        {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void sendButtonPressed(View v)
+    {
+        EditText mEdit = (EditText)findViewById(R.id.editText);
+
+        dataMap.getDataMap().putString("editTextValue", mEdit.getText().toString());
+        PutDataRequest request = dataMap.asPutDataRequest();
+        PendingResult<DataApi.DataItemResult> pendingResult = Wearable.DataApi
+                .putDataItem(mGoogleApiClient, request);
     }
 }
--- wear/src/main/java/net/austinhughes/speedreaderforwear/MainActivityWear.java
+++ wear/src/main/java/net/austinhughes/speedreaderforwear/MainActivityWear.java
@@ -3,14 +3,26 @@ package net.austinhughes.speedreaderforwear;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.wearable.view.WatchViewStub;
+import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.wearable.DataEvent;
+import com.google.android.gms.wearable.DataEventBuffer;
+import com.google.android.gms.wearable.PutDataMapRequest;
+import com.google.android.gms.wearable.Wearable;
 
 public class MainActivityWear extends Activity {
 
     private TextView mTextView;
+    private GoogleApiClient mGoogleApiClient;
+    private PutDataMapRequest dataMap;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_activity_wear);
         final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
@@ -20,5 +32,44 @@ public class MainActivityWear extends Activity {
                 mTextView = (TextView) stub.findViewById(R.id.text);
             }
         });
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(new GoogleApiClient.ConnectionCallbacks()
+                {
+                    @Override
+                    public void onConnected(Bundle connectionHint)
+                    {
+                        Log.d("Wearable API", "onConnected: " + connectionHint);
+                        // Now you can use the Data Layer API
+                    }
+                    @Override
+                    public void onConnectionSuspended(int cause)
+                    {
+                        Log.d("Wearable API", "onConnectionSuspended: " + cause);
+                    }
+                })
+                .addOnConnectionFailedListener(new GoogleApiClient.OnConnectionFailedListener()
+                {
+                    @Override
+                    public void onConnectionFailed(ConnectionResult result)
+                    {
+                        Log.d("Wearable API", "onConnectionFailed: " + result);
+                    }
+                })
+                        // Request access only to the Wearable API
+                .addApi(Wearable.API)
+                .build();
+
+        dataMap = PutDataMapRequest.create("/data");
+    }
+
+    public void onDataChanged(DataEventBuffer dataEvents)
+    {
+        for (DataEvent event : dataEvents) {
+            if (event.getType() == DataEvent.TYPE_CHANGED) {
+                Toast toast = Toast.makeText(getBaseContext(), "Data Changed", Toast.LENGTH_LONG);
+                toast.show();
+            }
+        }
     }
 }
